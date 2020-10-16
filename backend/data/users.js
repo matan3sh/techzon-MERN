d6f6ec59b7e4618170908515b620c8a91f6a1033
Matan Shaviro <matan3sh@gmail.com>
2020-10-16 15:03:16 +0300

    Fix bug in backend

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,17 +4,17 @@ module.exports.users = [
   {
     name: 'Admin',
     email: 'admin@gmail.com',
-    password: bcrypt.hashSync('adminadmin', 10),
+    password: bcrypt.hashSync('', 10),
     isAdmin: true,
   },
   {
     name: 'matan3sh',
     email: 'matan3sh@gmail.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('', 10),
   },
   {
     name: 'Test',
     email: 'test@gmail.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('', 10),
   },
 ];
